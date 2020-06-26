5920bcad5c909968b4592ae2b802938af7e6ab96
yg4689 <pjohn4@horizon.csueastbay.edu>
2020-06-26 12:21:00 -0700

    problem 2

--- problemset1.py
+++ problemset1.py
@@ -1,8 +1,13 @@
-#Write a program that counts up the number of vowels contained in the string s. Valid vowels are: 'a', 'e', 'i', 'o', and 'u'.
-#For example, if s = 'azcbobobegghakl', your program should print:
-
+# Write a program that prints the number of times the string 'bob' occurs in s.
+# For example, if s = 'azcbobobegghakl', then your program should print
+substring = "bob"
 count=0
-for char in s:
-    if char=="a" or char=="e" or char=="i" or char=="o" or char=="u":
+start=0
+while start < len(s):
+    pos = s.find(substring,start)
+    if pos != -1:
         count+=1
-print("Number of vowels: "+str(count))
+        start=pos+1
+    else:
+        break
+print("Number of times bob occurs is: ",count)
