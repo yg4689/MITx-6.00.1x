00226433d31c99a55412981dc406a07afba00b93
yg4689 <pjohn4@horizon.csueastbay.edu>
2020-06-26 12:35:45 -0700

    problem 2

--- problemset1_2.py
+++ problemset1_2.py
@@ -1,26 +1,13 @@
-# Assume s is a string of lower case characters.
-#
-# Write a program that prints the longest substring of s in which the letters occur in alphabetical order. For example, if s = 'azcbobobegghakl', then your program should print
-#
-# Longest substring in alphabetical order is: beggh
-# In the case of ties, print the first substring. For example, if s = 'abcbcd', then your program should print
-
-
-temp=s[0]
-temp2=""
-n=0
-while n+1 <len(s):
-    if s[n+1]>=s[n]:
-        temp+=s[n+1]
-        n=n+1
+# Write a program that prints the number of times the string 'bob' occurs in s.
+# For example, if s = 'azcbobobegghakl', then your program should print
+substring = "bob"
+count=0
+start=0
+while start < len(s):
+    pos = s.find(substring,start)
+    if pos != -1:
+        count+=1
+        start=pos+1
     else:
-        if len(temp)>len(temp2):
-            temp2=temp
-            temp=""
-        n=n+1
-        temp=s[n]
-
-if len(temp)>len(temp2):
-    print(temp)
-else:
-    print(temp2)
+        break
+print("Number of times bob occurs is: ",count)
